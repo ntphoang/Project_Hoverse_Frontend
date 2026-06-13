67cd64db6369a0ae7d5663ceea7c8a931fbd2edc
Phi Hoang <ntphoang205@gmail.com>
2026-06-13 14:52:53 +0700

    feat(auth): connect Login UI with AuthController API using axios and useAuth

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import "./Auth.css";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import axiosClient from "../../api/axiosClient";
 
 const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -14,14 +22,27 @@ const Login = () => {
     if (error) setError("");
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
     if (!formData.email || !formData.password) {
       setError("Vui lòng nhập đầy đủ email và mật khẩu");
       return;
     }
-    console.log("Dữ liệu đăng nhập: ", formData);
-    alert("Dữ liệu hợp lệ!");
+
+    try {
+      setLoading(true);
+      setError("");
+      const responseData = await axiosClient.post("/auth/login", formData);
+      login(responseData);
+      navigate("/");
+    } catch (err) {
+      const serverMessage =
+        err.response?.data || "Tài khoản hoặc mật khẩu không chính xác!";
+      setError(serverMessage);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -30,13 +51,12 @@ const Login = () => {
         <h2 className="auth-title">Đăng nhập</h2>
 
         <form onSubmit={handleSubmit}>
-          {error && <div className="error-text"></div>}
-
           <div className="form-group">
             <label>Email</label>
             <input
               type="email"
               name="email"
+              disabled={loading}
               value={formData.email}
               onChange={handleInputChange}
               placeholder="abc@domain.com"
@@ -48,14 +68,28 @@ const Login = () => {
             <input
               type="password"
               name="password"
+              disabled={loading}
               value={formData.password}
               onChange={handleInputChange}
               placeholder="********"
             />
           </div>
 
+          {error && (
+            <div
+              className="error-text"
+              style={{
+                marginBottom: "15px",
+                textAlign: "center",
+                fontSize: "14px",
+              }}
+            >
+              {error}
+            </div>
+          )}
+
           <button type="submit" className="btn-submit">
-            Đăng nhập
+            {loading ? "Đang xác thực..." : "Đăng nhập"}
           </button>
         </form>
 
