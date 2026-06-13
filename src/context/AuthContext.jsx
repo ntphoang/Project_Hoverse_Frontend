dce187db4c220ad5b5c0c4add76ed253002bab7a
Phi Hoang <ntphoang205@gmail.com>
2026-06-13 13:34:25 +0700

    feat(auth): implement AuthContext for global authentication state

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const email = localStorage.getItem("email");
+    const role = localStorage.getItem("role");
+
+    if (token && email) {
+      setUser({ email, role });
+    }
+  }, []);
+
+  const login = (userData) => {
+    localStorage.setItem("token", userData.token);
+    localStorage.setItem("email", userData.email);
+    localStorage.setItem("role", userData.role);
+
+    setUser({ email: userData.email, role: userData.role });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("role");
+
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = ()=>{
+    return useContext(AuthContext);
+}
