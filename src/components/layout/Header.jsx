2ca62eba0de8285946512f57c35a1a186b7af78b
Phi Hoang <ntphoang205@gmail.com>
2026-06-16 09:09:34 +0700

    feat(ui): update Header to reat to AuthContext and implement logout logic

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,16 @@
 import "./Header.css";
+import { useAuth } from "../../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
     <header className="app-header">
       <div className="logo-container">
@@ -9,13 +19,23 @@ export default function Header() {
       </div>
 
       <nav className="nav-menu">
-        <a href="/" className="nav-link active">
-          Khám phá
-        </a>
-        <a href="/saved" className="nav-link">
-          Đã lưu
-        </a>
-        <button className="login-btn">Đăng nhập</button>
+        {user ? (
+          <div className="user-info ">
+            <span>
+              Xin chào, <b>{user.email.split("@")[0]}</b>!
+            </span>
+            <button className="logout-btn" onClick={handleLogout}>Đăng xuất</button>
+          </div>
+        ) : (
+          <div className="auth-links">
+            <Link to="/login">
+              <button className="login-btn">Đăng nhập</button>
+            </Link>
+            <Link to="/register">
+              <button className="logout-btn">Đăng ký</button>
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
