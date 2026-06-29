49b9ea2249c5aeaeff7eecb863214d83b732a055
Phi Hoang <ntphoang205@gmail.com>
2026-06-29 20:52:50 +0700

    feat(review): add function getReviewByPlace in reviewSerice.jsx

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -2,7 +2,19 @@ import axiosClient from "../api/axiosClient";
 
 const reviewService = {
   createReview: async (placeId, reviewData) => {
-    const response = await axiosClient.post(`/places/${placeId}/reviews`, reviewData);
+    const response = await axiosClient.post(
+      `/places/${placeId}/reviews`,
+      reviewData,
+    );
+    return response;
+  },
+  getReviewsByPlace: async (placeId, page=0, size=5) => {
+    const response = await axiosClient.get(`/places/${placeId}/reviews`, {
+      params: {
+        page: page,
+        size: size,
+      },
+    });
     return response;
   },
 };
