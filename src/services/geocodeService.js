c7889684ee28e6b3cdc58f9c4d7c44c8e954ce26
Phi Hoang <ntphoang205@gmail.com>
2026-07-08 19:27:35 +0700

    feat(place-create): crate file geocodeService to call backend

---
+++ src/services/geocodeService.js
@@ -0,0 +1,15 @@
+import axiosClient from "../api/axiosClient";
+
+const geocodeService = {
+  reverseGeocode: async (latitude, longitude) => {
+    const response = await axiosClient.get("/reverse", {
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+    });
+    return response;
+  },
+};
+
+export default geocodeService;
