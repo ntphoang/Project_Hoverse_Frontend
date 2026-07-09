2371bc4680583eb8d0ee277833406f82f147262e
Phi Hoang <ntphoang205@gmail.com>
2026-07-09 23:00:14 +0700

    feat(place-create): update Header of axiosClient

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: "http://localhost:8088/api/v1",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 axiosClient.interceptors.request.use(
