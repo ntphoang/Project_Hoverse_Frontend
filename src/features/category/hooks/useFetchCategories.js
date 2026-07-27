00d6b471242a9d0ffd375bd1580ebae5aa03bf29
Phi Hoang <ntphoang205@gmail.com>
2026-07-27 08:46:34 +0700

    feat/place-update: create custom hook useFetchCategories

---
+++ src/features/category/hooks/useFetchCategories.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import categoryService from "../services/categoryService";
+
+const useFetchCategories = () => {
+  const [categories, setCategories] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchCategories = async () => {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await categoryService.getAllCategories();
+      setCategories(response);
+    } catch (error) {
+      setError("Có lỗi khi tải danh mục " + error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return { categories, isLoading, error };
+};
+
+export default useFetchCategories;
