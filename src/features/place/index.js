1110c6c1bc57aaa552fc0fb323a13cdf7bb007ff
Phi Hoang <ntphoang205@gmail.com>
2026-07-29 09:07:27 +0700

    feat/place-tag-created-filter: export useFetchPlaces and usePlaceFilter

--- src/features/place/index.js
+++ src/features/place/index.js
@@ -1,10 +1,12 @@
 export { default as AddPlaceModal } from "./components/AddPlaceModal";
 export { default as PlaceItem } from "./components/PlaceItem";
 export { default as MapPicker } from "./components/MapPicker";
+export { default as SearchPlace } from "./components/SearchPlace";
 
 export { default as PlaceDetail } from "./pages/PlaceDetail";
 export { default as PlaceUpdate } from "./pages/PlaceUpdate";
 
-export { default as usePlaces } from "./hooks/usePlaces";
+export { default as useFetchPlaces } from "./hooks/useFetchPlaces";
+export { default as usePlaceFilter } from "./hooks/usePlaceFilter";
 
 export { default as placeService } from "./services/placeService";
