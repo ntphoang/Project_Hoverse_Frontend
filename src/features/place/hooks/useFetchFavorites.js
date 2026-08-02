5cf63da1f128fc56583805b347246984ab6e1672
Phi Hoang <ntphoang205@gmail.com>
2026-08-02 20:25:31 +0700

    feat/favorite-page: create custom hooks as useFetchFavoriteIds and useFetchFavorites

--- src/features/place/hooks/useFetchFavorites.js
+++ src/features/place/hooks/useFetchFavorites.js
@@ -1,22 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import placeFavoriteService from "../services/placeFavoriteService";
 import { useFavoritesStore } from "@/store";
 
-const useFetchFavorites = () => {
-  const setFavoriteIds = useFavoritesStore(state=>state.setFavoriteIds);
+const PAGE_SIZE = 6;
 
-  const fetchFavorites = async () => {
+const useFetchFavorites = () => {
+  const favorites = useFavoritesStore((state) => state.favorites);
+  const setFavorites = useFavoritesStore((state) => state.setFavorites);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(false);
+
+  const fetchFavorites = async (currentPage) => {
     try {
-      const response = await placeFavoriteService.getPlaceFavorite();
-      setFavoriteIds(response);
+      setIsLoading(true);
+      setError(false);
+      const response = await placeFavoriteService.getPlaceFavorites(
+        currentPage,
+        PAGE_SIZE,
+      );
+      if (currentPage > 0) {
+        setFavorites([...favorites, ...response.content]);
+      } else {
+        setFavorites(response.content);
+      }
+      setHasMore(!response.last);
     } catch (error) {
-      console.log("Có lỗi xảy ra khi load favorites: ", error);
+      setError("Có lỗi xảy ra khi tải địa điểm yêu thích: " + error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  const handleHasMore = () => {
+    setPage((prev) => prev + 1);
+  };
+
   useEffect(() => {
-    fetchFavorites();
-  }, []);
+    fetchFavorites(page);
+  }, [page]);
+
+  return { favorites, isLoading, error, hasMore, handleHasMore };
 };
 
 export default useFetchFavorites;
