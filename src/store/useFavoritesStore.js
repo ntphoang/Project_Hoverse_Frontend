f57e8d1b353f709e5f3946499347655d50a10857
Phi Hoang <ntphoang205@gmail.com>
2026-08-02 20:26:31 +0700

    feat/favorite-page: add state favorites in useFavoritesStore

--- src/store/useFavoritesStore.js
+++ src/store/useFavoritesStore.js
@@ -3,28 +3,42 @@ import { create } from "zustand";
 
 const useFavoritesStore = create((set, get) => ({
   favoriteIds: [],
+  favorites: [],
 
   setFavoriteIds: (idList) => {
     set({ favoriteIds: idList });
   },
 
-  toggleFavorite: async (placeId) => {
-    const currentFavorites = get().favoriteIds;
-    const isSaved = currentFavorites.includes(placeId);
+  setFavorites: (placeList) => {
+    set({ favorites: placeList });
+  },
+
+  toggleFavorite: async (place) => {
+    const currentFavoriteIds = get().favoriteIds;
+    const currentFavorites = get().favorites;
+    const isSaved = currentFavoriteIds.includes(place.id);
 
     // Lưu trên FE
     if (isSaved) {
-      set({ favoriteIds: currentFavorites.filter((id) => id !== placeId) });
+      set({
+        favoriteIds: currentFavoriteIds.filter((id) => id !== place.id),
+        favorites: currentFavorites.filter(
+          (favorite) => favorite.id !== place.id,
+        ),
+      });
     } else {
-      set({ favoriteIds: [...currentFavorites, placeId] });
+      set({
+        favoriteIds: [...currentFavoriteIds, place.id],
+        favorites: [...currentFavorites, place],
+      });
     }
 
     // Lưu dưới BE
     try {
-      await placeService.toggleFavorite(placeId);
+      await placeService.toggleFavorite(place.id);
     } catch (error) {
       console.log("Có lỗi xảy ra khi toggle favorite: ", error);
-      set({ favoriteIds: currentFavorites }); //rollback
+      set({ favoriteIds: currentFavoriteIds, favorites: currentFavorites }); //rollback
     }
   },
 }));
