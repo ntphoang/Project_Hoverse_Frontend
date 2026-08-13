b5956190a5da9590fa00f44f5c6dd475962c7da1
Phi Hoang <ntphoang205@gmail.com>
2026-08-13 18:23:33 +0700

    feat/user-manage: use refresh token to handle accessToken expired

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: "http://localhost:8088/api/v1",
+  withCredentials: true,
 });
 
 axiosClient.interceptors.request.use(
@@ -22,7 +23,28 @@ axiosClient.interceptors.response.use(
   (response) => {
     return response.data;
   },
-  (error) => {
+  async (error) => {
+    const update = useAuthStore.getState().update;
+
+    const requestOld = error.config;
+
+    if (error.response.status === 401 && !requestOld._retry) {
+      requestOld._retry = true;
+
+      try {
+        const response = await axios.post("/api/v1/auth/refresh-token");
+
+        update({ token: response.data.token });
+        requestOld.headers.Authorization = `Bearer ${response.data.token}`;
+
+        return axiosClient(requestOld);
+      } catch (refreshError) {
+        useAuthStore.getState().logout();
+
+        return Promise.reject(refreshError);
+      }
+    }
+
     console.error("Lỗi từ Server: ", error.response?.data);
     return Promise.reject(error);
   },
