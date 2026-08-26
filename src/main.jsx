8a6210a5b6361ddbf1086547c76886ffa69f36ee
Phi Hoang <ntphoang205@gmail.com>
2026-08-26 08:24:34 +0700

    fix: not found markerIcon

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const queryClient = new QueryClient();
 
